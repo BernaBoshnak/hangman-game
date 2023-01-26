6e6e0f2cfb41f44786373057273fcef28806ee06
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2023-01-26 22:38:30 +0200

    create keyboard and key components and when a button is pressed, the clicked button becomes disabled

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Character from './components/Character'
 import Gallows from './components/Gallows'
-import styles from './App.module.scss'
+import Keyboard, { KeyboardLetter } from './components/Keyboard'
+import { wordsList } from './components/wordsList'
 import './styles/index.scss'
+import styles from './App.module.scss'
 
 function App() {
+  const word = wordsList[Math.floor(Math.random() * wordsList.length)]
+  const [pressedKeys, setPressedKeys] = useState<KeyboardLetter[]>([])
+
+  const handleKeyboardClick = (letter: KeyboardLetter) => {
+    setPressedKeys([...pressedKeys, letter])
+  }
+
   return (
     <div className={styles['wrapper']}>
       <div className={styles['left-col']}>
         Left col
+        <Character />
+        <Keyboard onKeyClick={handleKeyboardClick} pressedKeys={pressedKeys} />
       </div>
       <div className={styles['right-col']}>
         <div style={{ textAlign: 'center', fontSize: '2rem' }}>Win || Lose</div>
