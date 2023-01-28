e70acede97283106b8a85a1c22d645a8c58e864c
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2023-01-28 15:49:09 +0200

    implement key press event listener and adding pressed keys to state using the useEffect hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React, { useState } from 'react'
-import Character from './components/Character'
-import Gallows from './components/Gallows'
-import Keyboard, { KeyboardLetter } from './components/Keyboard'
+import React, { useState, useEffect } from 'react'
+import Character from './components/Character/Character'
+import Gallows from './components/Gallows/Gallows'
+import Word from './components/Word'
+import Keyboard, {
+  KeyboardLetter,
+  isKeyboardLetter,
+} from './components/Keyboard/Keyboard'
 import { wordsList } from './components/wordsList'
 import './styles/index.scss'
 import styles from './App.module.scss'
 
 function App() {
-  const word = wordsList[Math.floor(Math.random() * wordsList.length)]
+  // const word = wordsList[Math.floor(Math.random() * wordsList.length)]
+  const word = 'react'
+  const [wordToGuess, setWordToGuess] = useState(word.toUpperCase())
   const [pressedKeys, setPressedKeys] = useState<KeyboardLetter[]>([])
 
   const handleKeyboardClick = (letter: KeyboardLetter) => {
     setPressedKeys([...pressedKeys, letter])
   }
 
+  useEffect(() => {
+    function callback(e: KeyboardEvent) {
+      const key = e.key.toUpperCase()
+
+      if (isKeyboardLetter(key)) {
+        setPressedKeys((prevState) => [...prevState, key])
+      }
+    }
+
+    document.addEventListener('keypress', callback)
+
+    // cleanup
+    return () => {
+      document.removeEventListener('keypress', callback)
+    }
+  }, [])
+
   return (
     <div className={styles['wrapper']}>
       <div className={styles['left-col']}>
         Left col
         <Character />
+        <Word word={wordToGuess} pressedKeys={pressedKeys} />
         <Keyboard onKeyClick={handleKeyboardClick} pressedKeys={pressedKeys} />
       </div>
       <div className={styles['right-col']}>
