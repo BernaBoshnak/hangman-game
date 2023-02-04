6c596ee6cbadb9bd1aad0b3ba452a8005499f4db
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2023-02-04 20:00:32 +0200

    аdded unlock prop to display full or masked word based on its value

--- src/components/Word.tsx
+++ src/components/Word.tsx
@@ -5,13 +5,18 @@ import styles from './Word.module.scss'
 type WordProps = {
   word: string
   pressedKeys: KeyboardLetter[]
+  unlock: boolean
 }
 
-const Word = ({ word, pressedKeys }: WordProps) => {
-  const wordToShow = word
-    .split('')
-    .map((letter) => (pressedKeys.some((key) => key === letter) ? letter : '_'))
-    .join(' ')
+const Word = ({ word, pressedKeys, unlock }: WordProps) => {
+  const wordToShow = unlock
+    ? word
+    : word
+        .split('')
+        .map((letter) =>
+          pressedKeys.some((key) => key === letter) ? letter : '_',
+        )
+        .join(' ')
 
   return <div className={styles.container}>{wordToShow}</div>
 }
