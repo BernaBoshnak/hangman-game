6a60725ee65c386538a878fda503cc4b92a73fa0
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2023-02-04 19:47:20 +0200

    added ErrorMessage component to display error message in case of failure

---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './ErrorMessage.module.scss'
+
+type ErrorMessageProps = {
+  children: React.ReactNode
+}
+
+const ErrorMessage = ({ children }: ErrorMessageProps) => (
+  <div className={styles.alert} role="alert">
+    {children}
+  </div>
+)
+
+export default ErrorMessage
