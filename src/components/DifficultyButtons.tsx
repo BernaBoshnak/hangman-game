02133601f24d96e1b20ebb782da6bd47b14093b7
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2023-02-04 19:55:12 +0200

    аdded DifficultyButtons component to select difficulty level

---
+++ src/components/DifficultyButtons.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Level } from '../word'
+import styles from './DifficultyButtons.module.scss'
+
+type DifficultyButtonsProps = {
+  difficultyLevelClick: (difficulty: Level) => void
+}
+
+const DifficultyButtons = ({
+  difficultyLevelClick,
+}: DifficultyButtonsProps) => (
+  <div className={styles.buttonsContainer}>
+    <button
+      className={styles.button}
+      onClick={() => difficultyLevelClick('easy')}
+    >
+      Easy
+    </button>
+    <button
+      className={styles.button}
+      onClick={() => difficultyLevelClick('normal')}
+    >
+      Normal
+    </button>
+    <button
+      className={styles.button}
+      onClick={() => difficultyLevelClick('hard')}
+    >
+      Hard
+    </button>
+  </div>
+)
+
+export default DifficultyButtons
