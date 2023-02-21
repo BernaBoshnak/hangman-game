2a2f124a50d8aa7894e64f13b6f5dd3f342b823d
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2023-02-21 17:34:12 +0200

    added logic to handle objects where the keys are class names and their values indicate whether the class should be added to the result string

--- src/utils/classnames.ts
+++ src/utils/classnames.ts
@@ -1,9 +1,19 @@
-function classnames(...args: unknown[]) {
+type Args = (string | Record<string, boolean>)[]
+
+function classnames(...args: Args) {
   const result: string[] = []
 
   for (const arg of args) {
     if (typeof arg === 'string') {
       result.push(arg)
+    }
+
+    if (typeof arg === 'object' && arg) {
+      for (const [className, condition] of Object.entries(arg)) {
+        if (condition) {
+          result.push(className)
+        }
+      }
     }
   }
 
