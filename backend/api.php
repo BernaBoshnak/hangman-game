cc914c1e2c03d9d18e2a688fb672d9ecdec2fbc0
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2023-02-27 11:01:18 +0200

    add helper functions in a new utils.php file, exporting parts of the code into them, and improving the readability and clarity of the code, and also made changes to the httpResponse function to improve its functionality and performance

--- backend/api.php
+++ backend/api.php
@@ -2,82 +2,8 @@
 // error_reporting(0); // disable errors on prod
 error_reporting(-1); // enable errors on dev
 header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json; charset=utf-8');
 
-function httpResponse(int $statusCode, bool $isOk, array ...$params)
-{
-    http_response_code($statusCode);
-    $responseArray = ['ok' => $isOk, ...$params];
-
-    echo json_encode($responseArray);
-    exit;
-}
-
-function isInteger($input)
-{
-    return (ctype_digit(strval($input)));
-}
-
-/**
- * @param array $dataArr
- * @param int|string|null $filter
- * @param callable $arrayFiltersCallback
- * @return array
- */
-
-function filterBy($dataArr, $filter, $arrayFiltersCallback)
-{
-    if ($filter === null) {
-        return $dataArr;
-    }
-
-    return array_filter($dataArr, $arrayFiltersCallback);
-}
-
-function validateParamKeys()
-{
-    $validKeys = ['minLength', 'maxLength', 'startsWith', 'endsWith', 'contains'];
-    $requestParams = array_keys($_GET);
-    $invalidParams = array_diff($requestParams, $validKeys);
-
-    if (count($invalidParams)) {
-        httpResponse(400, false, ['message' => 'Invalid query params!', 'parameters' => $invalidParams]);
-    }
-}
-
-function validateParamValues($paramName, $type, &$errors)
-{
-    if (!isset($_GET[$paramName])) {
-        return null;
-    }
-
-    $value = $_GET[$paramName];
-
-    if (empty($value)) {
-        $errors[$paramName] = 'empty value';
-        return null;
-    }
-
-    if ($type === 'integer') {
-        if (isInteger($value)) {
-            return (int) $value;
-        }
-
-        $errors[$paramName] = 'integer expected';
-        return null;
-    }
-
-    if ($type === 'string') {
-        if (ctype_alpha($value)) {
-            return $value;
-        }
-
-        $errors[$paramName] = 'string expected';
-        return null;
-    }
-
-    return null;
-}
+include_once 'utils.php';
 
 // Validate query params - stop execution and return wrong keys
 validateParamKeys();
@@ -94,10 +20,10 @@ if (count($errors)) {
 }
 
 // Load the noun list
-const WORDS_STR = file_get_contents('nounlist.json');
-const WORDS = json_decode(WORDS_STR, true);
+$json = file_get_contents('nounlist.json');
+$jsonData = json_decode($json, true);
 
-$filteredData = WORDS;
+$filteredData = $jsonData;
 $filteredData = filterBy($filteredData, $minLength, fn($v) => strlen($v) >= $minLength);
 $filteredData = filterBy($filteredData, $maxLength, fn($v) => strlen($v) <= $maxLength);
 $filteredData = filterBy($filteredData, $startsWith, fn($v) => strpos($v, $startsWith) === 0);
