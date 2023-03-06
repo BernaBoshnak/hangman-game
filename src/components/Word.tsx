b88bcf3b574684eb2fc6043766a0c32b6cb3a7da
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2023-03-06 20:11:43 +0200

    refactor word component to improve letter rendering and add styles for correct and incorrect keys

--- src/components/Word.tsx
+++ src/components/Word.tsx
@@ -9,36 +9,36 @@ type WordProps = {
   unlock: boolean
 }
 
-const Word = ({ word, pressedKeys, unlock }: WordProps) => {
-  const wordToShow = unlock
-    ? word.split('').map((letter, index) => (
-        <span key={index} className={styles.letter}>
+const generateLetterElement = (letter: string) => {
+  return (
+    <span className={styles.letter}>
+      <svg viewBox="0 0 50 25" className={styles['letter-svg']}>
+        <text x="50%" y="70%" dominantBaseline="middle" textAnchor="middle">
           {letter}
-        </span>
-      ))
-    : word.split('').map((letter, index) => (
-        <span
-          key={index}
-          className={cn(styles['letter-wrapper'], {
-            [styles.correct]: pressedKeys.some((key) => key === letter),
-          })}
-        >
-          {pressedKeys.some((key) => key === letter) && (
-            <span className={styles.letter}>
-              <svg viewBox="0 0 50 25" className={styles['letter-svg']}>
-                <text
-                  x="50%"
-                  y="70%"
-                  dominantBaseline="middle"
-                  textAnchor="middle"
-                >
-                  {letter}
-                </text>
-              </svg>
-            </span>
-          )}
-        </span>
-      ))
+        </text>
+      </svg>
+    </span>
+  )
+}
+
+const Word = ({ word, pressedKeys, unlock }: WordProps) => {
+  const wordToShow = word.split('').map((letter, index) => {
+    const isCorrect = pressedKeys.some((key) => key === letter)
+    const isIncorrect = unlock && !isCorrect
+    const letterElement =
+      isCorrect || isIncorrect ? generateLetterElement(letter) : null
+    return (
+      <span
+        key={index}
+        className={cn(styles['letter-wrapper'], {
+          [styles['letter-wrapper-correct']]: isCorrect,
+          [styles['letter-wrapper-incorrect']]: isIncorrect,
+        })}
+      >
+        {letterElement}
+      </span>
+    )
+  })
 
   return <div className={styles.container}>{wordToShow}</div>
 }
