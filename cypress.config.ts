7d85c2b0f88944ab73c9d14a2bb7a78b3c3b8367
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2025-04-09 22:48:37 +0300

    add webpack overrides for cypress tests, install loaders and setup .env.test

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,46 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  video: false,
   component: {
     specPattern: './tests/**/*.cy.{ts,tsx}',
     devServer: {
       framework: 'create-react-app',
       bundler: 'webpack',
+      webpackConfig: {
+        mode: 'test',
+        devtool: false,
+        module: {
+          rules: [
+            {
+              test: /\.(ts|tsx)$/,
+              use: [
+                {
+                  loader: 'babel-loader', // Transpile ES6+ to ES5
+                  options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react'],
+                    plugins: [
+                      [
+                        '@babel/plugin-transform-modules-commonjs',
+                        { loose: true },
+                      ],
+                    ],
+                  },
+                },
+                {
+                  loader: 'ts-loader', // Compile TypeScript to JavaScript
+                  options: {
+                    transpileOnly: true, // Enable transpilation only mode
+                  },
+                },
+              ],
+            },
+          ],
+        },
+        resolve: {
+          extensions: ['.tsx', '.ts', '.js', '.jsx', '.json', '.scss'],
+        },
+      },
     },
   },
 })
